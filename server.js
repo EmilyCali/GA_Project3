48d62dad854a8ba43bb1b1cdf842c70a5f5f2382
Amanda Capella <mrs.acapella@gmail.com>
2017-04-30 19:10:19 +0200

    heroku in server.js set up. controllers/users.js begun, models/users.js begun. some node modules required, but not installed.

--- server.js
+++ server.js
@@ -2,21 +2,33 @@
 var express = require('express');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
+var expressJWT = require('express-jwt');
+var jwt = require('jsonwebtoken');
 
 var app = express();
+
+//-------------------Environment Variables for Heroku
+var port = process.env.PORT || 3000;
+var mongoDBURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/beerbooks';
 
 //-------------------------Middleware
 
 app.use(bodyParser.json());
 app.use(express.static('public'));
+app.use(expressJWT({secret: 'peanutbutterjellytime'}).unless({path:['/login']}));
+
+//-------------------Controller Middleware
+var usersController = require('./controllers/users.js');
+app.use('/users', usersController);
 
 //----------------Connecting to Mongo
-mongoose.connect('mongodb://localhost:27017/beerbooks');
+mongoose.connect(mongoDBURI);
 mongoose.connection.once('open', function(){
     console.log("===================");
     console.log("--Mongo--Connected-");
     console.log("===================");
 });
+
 
 //-----------------Nodemon Connection
 app.listen(3000, function(){
