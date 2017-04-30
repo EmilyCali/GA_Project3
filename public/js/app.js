da3655f5fefc0ec6d66ba531911d7330b64f6655
Amanda Capella <mrs.acapella@gmail.com>
2017-04-30 09:39:10 +0200

    Notes from JWT site added

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,43 @@
 var app = angular.module('BBApp', []);
+//***********************************************************
+//************************************************************
+//according to https://www.w3.org/wiki/CORS_Enabled I need to put the app.all below in our code...Thom's tutorial for JWT say to "enable CORS"--this site further says we need to do this for all our routes:
+// app.get('/', function(req, res, next) {
+//     // Handle the get for this route
+//   });
+//   app.post('/', function(req, res, next) {
+//     // Handle the post for this route
+//   });
+//************************************************************
+//************************************************************
+// app.use(function(req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Headers", "X-Requested-With");
+//     next()
+// });
 
 app.controller('MainController', ['$http', function($http){
     var controller = this;
 
-
+    //******************JWT login stuffs
+    this.url = 'http://localhost:3000';
+    this.user = {};
+    //login(userInfo) function
+    this.login = function(userInfo){
+        // console.log(userInfo);
+        //make a POST request to the login route and log the response
+        $http({
+            method: 'POST',
+            url: this.url + '/users',
+            data: {
+                user: {
+                    username: userInfo.username,
+                    password: userInfo.password
+                }
+            }
+        }).then(function(response) {
+            console.log(response);
+            this.user = response.data.user;
+        }.bind(this));
+    };
 }]);
--- server.js
+++ server.js
@@ -2,16 +2,26 @@
 var express = require('express');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
+var jwt = require('jwt-express');
 
 var app = express();
+
+//-------------------Environment Variables for Heroku
+var port = process.env.PORT || 3000;
+var mongoDBURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/beerbooks';
 
 //-------------------------Middleware
 
 app.use(bodyParser.json());
 app.use(express.static('public'));
+app.use(jwt.init('peanutbutterjellytime'));
+
+//-------------------Controller Middleware
+var usersController = require('./controllers/users.js');
+app.use('/users', usersController);
 
 //----------------Connecting to Mongo
-mongoose.connect('mongodb://localhost:27017/beerbooks');
+mongoose.connect(mongoDBURI);
 mongoose.connection.once('open', function(){
     console.log("===================");
     console.log("--Mongo--Connected-");
