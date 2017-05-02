244899d342772c1b3930af9ffb2c05c18e267b44
Amanda Capella <mrs.acapella@gmail.com>
2017-05-02 19:53:11 +0200

    updates after nuclear

--- public/js/app.js
+++ public/js/app.js
@@ -1,61 +1,29 @@
 var app = angular.module('BBApp', []);
 
-//////////////////////////////////////////|
-//----------------Joe's controller--------|
-//////////////////////////////////////////|
-
-app.controller('baseCtrl', ['$http', function($http){
-    var controller = this;
-    this.token = false;
-    this.searching = '';
-    this.beers = [];
-
-    this.find = function(){
-        $http(
-            {
-                method:'GET',
-                url: 'https://api.brewerydb.com/v2/search?q=' + this.searching + '&type=beer&key=3553963f6fa0d83f188f21fcc4ac9343&format=json'
-            }).then(
-            function(response) { //success callback
-                controller.beers = [];
-
-                for(i=0; i< response.data.data.length; i++)
-                {
-                    controller.beers.push(response.data.data[i]);
-                }
-
-                console.log('success');
-                console.log(controller.nameArr);
-                console.log(controller.beers);
-                console.log(response);
-                console.log(controller.infoArr);
-            },
-            function(error){ //fail callback
-                console.log('fail');
-                console.log(error);
-            }
-        );
-    };
-    this.showInfo = function(index){
-        this.showBeerId = index;
-    }
-}]);
 
 //////////////////////////////////////////|
 //----------------Amanda's controller-----|
 //////////////////////////////////////////|
-app.controller('MainController', ['$http', function($http){
+app.controller('MainController', ['$scope', '$http', function($scope, $http){
     var controller = this;
     this.url = 'http://locallhost:3000/';
     this.user = {};
     this.token = false;
+    $scope.token = this.token;
 
-    this.secretStuff = function(){
-        console.log(localStorage);
-    };
+    $scope.$watch('token', function(newValue, oldValue){
+        console.log(newValue, oldValue);
+        this.sendToken = function(){
+            $scope.$broadcast('tokenChange', { token: controller.token});
+        };
+        sendToken();
+    });
+
+    // this.sendToken = function(){
+    //     $scope.$broadcast('eventName', { someProperty:'someValue'});
+    // };
 
     this.signUp = function(userInfo){
-        console.log(userInfo);
         $http({
             method:'POST',
             url: "/api/signup",
@@ -68,7 +36,7 @@ app.controller('MainController', ['$http', function($http){
             controller.username = response.data.user.username;
             // console.log(controller.user);
             localStorage.setItem('token', JSON.stringify(response.data.token));
-
+            //post request to authenticate newly registered user
             $http({
                 method:'POST',
                 url: "/api/authenticate",
@@ -79,13 +47,8 @@ app.controller('MainController', ['$http', function($http){
                     // }
                 }
             }).then(function(response){
-
-
                 controller.token = true;
-
-
-
-
+                $scope.token = controller.token;
             });
 
         }.bind(this));
@@ -104,20 +67,21 @@ app.controller('MainController', ['$http', function($http){
                 // }
             }
         }).then(function(response){
-            console.log(response);
+
             controller.username = response.data.username;
             controller.token = true;
-            console.log(response.data.message);
+            $scope.token = controller.token;
+
             if(!response.data.success){
-                controller.message = response.data.message;
                 controller.token = false;
+                $scope.token = controller.token;
+                controller.message = response.data.message;
             };
             localStorage.setItem('token', JSON.stringify(response.data.token));
         }.bind(this));
     };
 
     this.getUsers = function(){
-        console.log("in get");
         $http({
             // url: this.url + 'api/users',
             url: '/api/users',
@@ -126,12 +90,10 @@ app.controller('MainController', ['$http', function($http){
                 Authorization: JSON.parse(localStorage.getItem('token'))
             }
         }).then(function(response){
-            console.log(response);
             if (response.data.status == 401) {
                 this.error = "Unauthorized";
             } else {
                 controller.users = response.data;
-                console.log(controller.users);
             }
         }.bind(this));
 
@@ -144,10 +106,71 @@ app.controller('MainController', ['$http', function($http){
 }]);
 
 //////////////////////////////////////////|
-//-----------------Book controller-----|
+//----------------Joe's controller--------|
 //////////////////////////////////////////|
 
-app.controller("BookController", ["$http", function($http) {
+app.controller('baseCtrl', ['$scope','$http', function($scope, $http){
+    var controller = this;
+    this.token = false;
+    this.searching = '';
+    this.nameArr = [];
+    this.infoArr = [];
+    this.descriptionArr = [];
+
+    $scope.$on('tokenChange', function(event, data){
+		if(!data.token){
+            controller.token = false;
+        } else if(data.token){
+            controller.token = true;
+        };
+        console.log(controller.token)
+	});
+
+
+    this.find = function(){
+        $http(
+            {
+                method:'GET',
+                url: 'https://api.brewerydb.com/v2/search?q=' + this.searching + '&type=beer&key=3553963f6fa0d83f188f21fcc4ac9343&format=json'
+            }).then(
+            function(response) { //success callback
+                controller.nameArr = [];
+                controller.infoArr = [];
+                controller.descriptionArr = [];
+
+                for(i=0; i< response.data.data.length; i++)
+                {
+
+
+                    // controller.arr.push(response.data.data[i].name);
+                    controller.nameArr.push(response.data.data[i].name);
+                    controller.infoArr.push(response.data.data[i].id)
+                    controller.descriptionArr.push(response.data.data[i].style.description)
+                }
+
+                console.log('success');
+                console.log(controller.nameArr);
+                console.log(response);
+                console.log(controller.infoArr);
+            },
+            function(error){ //fail callback
+                console.log('fail');
+                console.log(error);
+            }
+        );
+    };
+    this.showInfo = function(index){
+        console.log(index);
+        console.log(controller.infoArr[index]);
+        this.showBeerId = index;
+    }
+}]);
+
+//////////////////////////////////////////|
+//-----------------Book controller--------|
+//////////////////////////////////////////|
+
+app.controller("BookController", ["$scope","$http", function($scope, $http) {
     //added by Amanda to check if user is logged in or not
     this.token = false;
   //have to name controller so it can be used in callbacks
@@ -167,7 +190,7 @@ app.controller("BookController", ["$http", function($http) {
       controller.foundBooks = [];
       for(i=0; i< 10; i++)
       {
-          controller.foundBooks.push(response.data.docs[i]);
+          controller.foundBooks.push(response.data.docs[i].title);
       }
       console.log(response);
       // let me see what this is
