1dea4cf3eb3597e14c59813b14594186090a85d8
emilycali <cali_emily@bah.com>
2017-05-02 11:58:13 -0400

    FIXED THE BOOKS BY CHECKING OUT THE OLD COMMIT THAT WORKED

--- public/js/app.js
+++ public/js/app.js
@@ -124,7 +124,7 @@ app.controller("BookController", ["$http", function($http) {
       controller.foundBooks = [];
       for(i=0; i< 10; i++)
       {
-          controller.foundBooks.push(response.data.docs[i].title);
+          controller.foundBooks.push(response.data.docs[i]);
       }
       console.log(response);
       // let me see what this is
