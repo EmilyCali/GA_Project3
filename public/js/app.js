dafa19c199fe427be8f8020d0ed59dadc81caf5e
Ciampije <josephciampi@C1MRQ06AHF4H.fios-router.home>
2017-05-04 15:20:50 -0400

    clickable on create a new pair

--- public/js/app.js
+++ public/js/app.js
@@ -240,13 +240,14 @@ app.controller('MainController', ['$scope', '$http', function($scope, $http){
 
     };
 
-    // this.returnToSearch = function(){
-    //     this.hideStuff = false;
-    //     this.showAllTheLikes = false;
-    //     this.showUsers = false;
-    //     controller.isSelected = true;
-    //
-    // }
+    this.returnToSearch = function(){
+        this.hideStuff = false;
+        this.showAllTheLikes = false;
+        this.showUsers = true;
+        this.isSelected = true;
+        this.getUsers();
+
+    }
 
 //////////////////////////////////////////|
 //----------------Joe's code--------------|
